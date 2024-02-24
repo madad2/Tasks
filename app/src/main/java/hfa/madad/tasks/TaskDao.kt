47ad40eeaf6d45dece45aa72fb35179f28541ee7
madad <alexander.laputko@ya.ru>
2024-02-24 15:57:07 +0300

    Created DAO for our database task_table using Room.

---
+++ app/src/main/java/hfa/madad/tasks/TaskDao.kt
@@ -0,0 +1,29 @@
+package hfa.madad.tasks
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Insert
+    fun insert(task: Task)
+
+    @Insert
+    fun insertAll(tasks: List<Task>)
+
+    @Update
+    fun update(task: Task)
+
+    @Delete
+    fun delete(task: Task)
+
+    @Query("SELECT * FROM task_table WHERE taskId = :taskId")
+    fun get(taskId: Long): LiveData<Task>
+
+    @Query("SELECT * FROM task_table ORDER BY taskId DESC")
+    fun getAll(): LiveData<List<Task>>
+}
