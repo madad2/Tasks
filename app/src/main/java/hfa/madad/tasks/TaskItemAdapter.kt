573637181154103d50807b0918baedd5a1c0d4d7
madad <alexander.laputko@ya.ru>
2024-02-27 20:19:57 +0300

    Added clickListener to RecycleView.

--- app/src/main/java/hfa/madad/tasks/TaskItemAdapter.kt
+++ app/src/main/java/hfa/madad/tasks/TaskItemAdapter.kt
@@ -4,12 +4,15 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import hfa.madad.tasks.databinding.TaskItemBinding
+import hfa.madad.tasks.generated.callback.OnClickListener
 
-class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
+class TaskItemAdapter(val clickListener: (taskID: Long) -> Unit) :
+    ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
 
     //Вызывается каждый раз, когда потребуется создать ViewHolder (держатель представления)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
@@ -19,7 +22,7 @@ class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(Ta
     //Вызывается, когда данные должны отображаться в ViewHolder (держателе представления)
     override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
         val item = getItem(position)
-        holder.bind(item)
+        holder.bind(item, clickListener)
     }
 
     //Определяет ViewHolder (держатель представления)
@@ -35,8 +38,12 @@ class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(Ta
         }
 
         //Данные добавляются в макет ViewHolder (держателя представления)
-        fun bind(item: Task) {
+        fun bind(item: Task, clickListener: (taskID: Long) -> Unit) {
             binding.task = item
+
+            binding.root.setOnClickListener {
+                clickListener(item.taskId)
+            }
         }
     }
 }
--- app/src/main/java/hfa/madad/tasks/TasksFragment.kt
+++ app/src/main/java/hfa/madad/tasks/TasksFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import hfa.madad.tasks.databinding.FragmentTasksBinding
@@ -30,7 +31,13 @@ class TasksFragment : Fragment() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        val adapter = TaskItemAdapter()
+        val adapter = TaskItemAdapter { taskID ->
+            Toast.makeText(
+                context,
+                "Clicked task $taskID",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         binding.tasksList.adapter = adapter
 
         //Наблюдаем за свойством tasks модели представления
